58b00e2d0f407ffa5c42877f335162074b3a6741
Rubén <rubencelislobato@gmauil.com>
2023-12-04 19:34:44 +0100

    Cambios

--- src/main/kotlin/advent_of_code_1.kt
+++ src/main/kotlin/advent_of_code_1.kt
@@ -2,27 +2,16 @@ fun main() {
     /**
      * Función principal donde se ejecuta el programa.
      */
-    val listaDeCadenas = listOf(
-        "1abc2",
-        "pqr3stu8vwx",
-        "a1b2c3d4e5f",
-        "treb7uchet"
-    )
 
-    val sumaDeValores = calcularCalibracionDeValores(listaDeCadenas)
+    val puzles = readInput("Day01")
+    val sumaDeValores = calcularCalibracionDeValores(puzles)
     println("La suma de todos los valores es: $sumaDeValores")
+
 }
 
 fun calcularCalibracionDeValores(listaDeCadenas: List<String>): Int {
-    /**
-     * Calcula la calibración de valores a partir de una lista de cadenas.
-     *
-     * @param listaDeCadenas Lista de cadenas que contienen valores de calibración.
-     * @return La suma de todos los valores de calibración.
-     */
-    // var sumaTotal = 0
-
     val listaDigitos = mutableListOf<Int>()
+
     for (linea in listaDeCadenas) {
         var numerosLinea = ""
         for (caracter in linea) {
@@ -30,10 +19,11 @@ fun calcularCalibracionDeValores(listaDeCadenas: List<String>): Int {
                 numerosLinea += caracter
             }
         }
-        var primerDigito = numerosLinea.first()
-        var segundoDigito = numerosLinea.last()
+        val primerDigito = numerosLinea.first()
+        val segundoDigito = numerosLinea.last()
         val digitoCreado = "$primerDigito$segundoDigito".toInt()
         listaDigitos.add(digitoCreado)
+
     }
 
     return listaDigitos.sum()
