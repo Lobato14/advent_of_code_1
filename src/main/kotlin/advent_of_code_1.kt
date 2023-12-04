bf07288ad0391fe70f174b05d69652b2158e96d9
Rubén <rubencelislobato@gmauil.com>
2023-12-04 16:24:32 +0100

    Creación y Realizacion del test_adventure_of_code

--- src/main/kotlin/advent_of_code_1.kt
+++ src/main/kotlin/advent_of_code_1.kt
@@ -2,27 +2,27 @@ fun main() {
     /**
      * Función principal donde se ejecuta el programa.
      */
-    val cadenaCalibrada = listOf(
+    val listaDeCadenas = listOf(
         "1abc2",
         "pqr3stu8vwx",
         "a1b2c3d4e5f",
         "treb7uchet"
     )
 
-    val sumaDeValores = calcularCalibracionDeValores(cadenaCalibrada)
+    val sumaDeValores = calcularCalibracionDeValores(listaDeCadenas)
     println("La suma de todos los valores es: $sumaDeValores")
 }
 
-fun calcularCalibracionDeValores(cadenaCalibrada: List<String>): Int {
+fun calcularCalibracionDeValores(listaDeCadenas: List<String>): Int {
     /**
      * Calcula la calibración de valores a partir de una lista de cadenas.
      *
-     * @param cadenaCalibrada Lista de cadenas que contienen valores de calibración.
+     * @param listaDeCadenas Lista de cadenas que contienen valores de calibración.
      * @return La suma de todos los valores de calibración.
      */
     var suma = 0
 
-    for (linea in cadenaCalibrada) {
+    for (linea in listaDeCadenas) {
         val primerDigito = linea.first().toString().toIntOrNull()
         val ultimoDigito = linea.last().toString().toIntOrNull()
 
---
+++ src/test/kotlin/test_advent_of_code_1.kt
@@ -0,0 +1,20 @@
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class CalibrationTest {
+
+    @Test
+    fun testcalcularCalibracionDeValores() {
+        val listaDeCadenas = listOf(
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet"
+        )
+
+        val resultado = calcularCalibracionDeValores(listaDeCadenas)
+
+        // Asumiendo que el resultado esperado es 142 según el ejemplo dado
+        assertEquals(12, resultado)
+    }
+}
